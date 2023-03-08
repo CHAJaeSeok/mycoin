894693c52030ef23104fafafda5b6df153259fde
Mason <cjs2338@naver.com>
2023-03-08 23:18:03 +0900

    2023.03.01

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from mybalances import balances
+import pyupbit
+from api_key import access_key
+from api_key import secret_key
+
+upbit = pyupbit.Upbit(access=access_key, secret=secret_key)
+
+price_KRW = pyupbit.get_current_price(["KRW-BTC", "KRW-ETH", "KRW-XRP", "KRW-ADA", "KRW-BCH", "KRW-MED", "KRW-DOGE"])
+
+KRW_BTC = int(price_KRW["KRW-BTC"]) * int(upbit.get_balance(ticker="KRW-BTC"))
+KRW_ETH = int(price_KRW["KRW-ETH"]) * int(upbit.get_balance(ticker="KRW-ETH"))
+KRW_XRP = int(price_KRW["KRW-XRP"]) * int(upbit.get_balance(ticker="KRW-XRP"))
+KRW_ADA = int(price_KRW["KRW-ADA"]) * int(upbit.get_balance(ticker="KRW-ADA"))
+KRW_BCH = int(price_KRW["KRW-BCH"]) * int(upbit.get_balance(ticker="KRW-BCH"))
+KRW_MED = int(price_KRW["KRW-MED"]) * int(upbit.get_balance(ticker="KRW-MED"))
+KRW_DOG = int(price_KRW["KRW-DOGE"]) * int(upbit.get_balance(ticker="KRW-DOGE"))
+
+balances = [KRW_BTC, KRW_ETH, KRW_XRP, KRW_ADA, KRW_BCH, KRW_MED, KRW_DOG]
+coin_name = ['KRW-BTC', 'KRW-ETH', 'KRW-XRP', 'KRW-ADA', 'KRW-BCH', 'KRW-MED', 'KRW-DOGE']
+
+num = 0
+for i in balances:
+    coinname = coin_name[num]
+    print(coinname[:7], "현재가 : {0:>10,} 원".format(int(price_KRW[coin_name[num]])), "평가금 : {0:>10,} 원".format(i),
+          "수량 : {0:>10,}".format(int(upbit.get_balance(ticker=coin_name[num]))))
+    num = num + 1
 
 
-for balance in balances:
- print(balance)
-
- for i in range(0, 18):
-  print(i, balances[i]['currency'], balances[i]['balance'])
-
- # 원화 잔고 조회
- print("보유 KRW : {}".format(upbit.get_balance(ticker="KRW")))  # 보유 KRW
- print("총매수금액 : {}".format(upbit.get_amount('ALL')))  # 총매수금액
- print("비트수량 : {}".format(upbit.get_balance(ticker="KRW-BTC")))  # 비트코인 보유수량
- print("리플 수량 : {}".format(upbit.get_balance(ticker="KRW-XRP")))  # 리플 보유수량
- print("\n")
- print(upbit.get_chance('KRW-BTC'))  # 마켓별 주문 가능 정보를 확인
- print("\n")
- print(upbit.get_order('KRW-XRP'))  # 주문 내역 조회
